e98580c9fa6ed62c3e2dfebe7eea36d87112085f
kikor7 <kiko2002@hotmail.es>
2023-12-06 00:58:53 +0100

    arreglado la gestion de sesion
    sobreescribias la lista de usuarios, se usó
      let listaUsuarios = JSON.parse(localStorage.getItem('listaUsuarios')) || []; para evitarlo

--- js/gestion-sesion.js
+++ js/gestion-sesion.js
@@ -4,14 +4,11 @@ let listaUsuarios = [];
 document.querySelector('.formRegistro').addEventListener('submit', e => {
     e.preventDefault(); //prevenimos que se recarge la pagina
     const data = Object.fromEntries(new FormData(e.target))
+    let listaUsuarios = JSON.parse(localStorage.getItem('listaUsuarios')) || [];
     listaUsuarios.push(data)
     localStorage.setItem('listaUsuarios', JSON.stringify(listaUsuarios));
-    console.log(listaUsuarios)
     window.location.href = "inicio-sesion.html";
     });
-
-
-
 
 //sacamos los .value del form de inicio de sesion
 document.querySelector('form').addEventListener('submit', e => {
@@ -19,19 +16,23 @@ document.querySelector('form').addEventListener('submit', e => {
     const data = Object.fromEntries(new FormData(e.target))
     let credenciales = localStorage.getItem('listaUsuarios');
     credenciales = JSON.parse(credenciales);
-    console.log(credenciales);
     for (let i = 0; i < credenciales.length; i++) {
         if (credenciales[i].correo === data.correo && credenciales[i].contraseña === data.contraseña) {
             window.location.href = "index.html";
             return true;
         }
     }
+    if(parrafo = document.querySelector(".quitarInfo")){
+        parrafo.remove();
+    }
+    
     let contenedor = document.querySelector(".tab-content");
     let p = document.createElement("p");
     p.textContent = "Usuario o contraseña incorrectos";
-   p.className = "text-end color-info-inicio-sesion";
+   p.className = "text-end color-info-inicio-sesion quitarInfo";
     contenedor.appendChild(p);
-    return false;
+    
     });
+
 
 
