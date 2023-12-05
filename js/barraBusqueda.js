7bda80986cb942008df362ee5b2093a255be26ce
kikor7 <kiko2002@hotmail.es>
2023-12-05 18:31:16 +0100

    arreglado lo de img repetida en busqueda

--- js/barraBusqueda.js
+++ js/barraBusqueda.js
@@ -9,27 +9,31 @@ document.addEventListener('keyup', (e) => {
 
             if (textoProducto.includes(textoBusqueda)) {
                 producto.classList.remove('filtro');
+                
             } else {
                 producto.classList.add('filtro');
                 contador++;
             }
-        });
+           }
+        );
+
+        if (div = document.querySelector('.quitar')){
+            div.remove();
+          }
 
         if (contador === productos.length) {
             const nuevoContenedor = document.querySelector(".contenido-productos");
             const div = document.createElement("div");
-            div.className = " container col-12 producto-detalles text-center";
+            div.className = " container col-12 producto-detalles text-center quitar";
 
             const img = document.createElement("img");
-            img.className = "img-fluid quitar";
+            img.className = "img-fluid ";
             img.src = "img/producto-no-encontrado.png";
 
             div.appendChild(img);
             nuevoContenedor.appendChild(div);
         }
-        if(contador === 0) {
-            var imagen = document.queryselector(".quitar");
-            imagen.remove();
-    }}
+        }
+        
 });
 
